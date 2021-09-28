75a14caf8629ae8eef0e3f99a3acee071ed2e294
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-09-28 16:50:32 -0300

    starting to consume components pluging store together

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { inject, observer } from 'mobx-react';
 import React from 'react';
-import { Input, Module } from './components';
+import { inject, observer } from 'mobx-react';
+import { InputGeneratorConfig, OperationModuleConfig, RunConfig } from './components';
+import VotingMethod from './lib/tmr/Voter/VotingMethod';
 import { StoreKeyNames } from './stores';
 import TMRStore from './stores/tmr.store';
-// import { inject, observer } from 'mobx-react';
+import { FunctionsKeys } from './utils';
 
 type Props = {
   tmrStore?: TMRStore;
 };
 
-const Demo: React.FC<Props> = ({ tmrStore }) => {
-  console.log(tmrStore);
+const votingMethodKeys = Object.keys(VotingMethod);
+
+const splitCorrectVotingMethodKeys = (): string[] => {
+  const resultantArray = votingMethodKeys.slice((votingMethodKeys.length / 2), votingMethodKeys.length);
+  return resultantArray;
+};
+
+const App: React.FC<Props> = ({ tmrStore }) => {
+
+  const handleOperationNameChange = ( operationName: FunctionsKeys ): void => {
+    tmrStore?.setOperationModuleConfig({ operationName });
+  };
+
+  const handleVotingMethodChange = ( votingMethod: number ): void => {
+    tmrStore?.setTMRRunConfig({ votingMethod })
+  };
+
+  const handleIterationsChange = ( iterations: number ): void => {
+    tmrStore?.setTMRRunConfig({ iterations })
+  };
+
   const handleClick:React.MouseEventHandler<HTMLButtonElement> | undefined = async () => {
     try {
       await tmrStore?.run();
@@ -30,10 +50,21 @@ const Demo: React.FC<Props> = ({ tmrStore }) => {
           justifyContent: 'center',
         }}
       >
-        <Input />
+        <InputGeneratorConfig />
+        <OperationModuleConfig
+          value={tmrStore?.operationModuleConfig.operationName}
+          handleOperationNameChange={handleOperationNameChange}
+        />
+        <RunConfig
+          handleVotingChange={handleVotingMethodChange}
+          votingValue={tmrStore?.runConfig.votingMethod || 0}
+          votingMethods={splitCorrectVotingMethodKeys()}
+          iterationsValue={tmrStore?.runConfig.iterations}
+          handleIterationChange={handleIterationsChange}
+        />
         <button onClick={handleClick}>Oii</button>
         <h1>Hello world!</h1>
-        <h4>{tmrStore?.moduleConfig.operationName}</h4>
+        <h4>{tmrStore?.inputGeneratorConfig.maximum}</h4>
       </div>
       <div
         style={{
@@ -44,7 +75,7 @@ const Demo: React.FC<Props> = ({ tmrStore }) => {
           justifyContent: 'center',
         }}
       >
-        <Module />
+        <OperationModuleConfig />
         <h1>Hello world!</h1>
       </div>
     </>
@@ -53,4 +84,4 @@ const Demo: React.FC<Props> = ({ tmrStore }) => {
 
 const tmr: StoreKeyNames = 'tmrStore';
 
-export default inject(tmr)(observer(Demo));
+export default inject(tmr)(observer(App));
