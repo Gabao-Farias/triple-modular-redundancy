b491d00c4a50151fd89ee130cc67d19026939028
Gabao-Farias <gabrielfariasbass@gmail.com>
2021-10-01 11:16:08 -0300

    results can be undefined

--- src/stores/tmr.store.ts
+++ src/stores/tmr.store.ts
@@ -34,7 +34,7 @@ export default class TMRStore {
 
   @persist('object')
   @observable
-  results: ModuleIterationResultType[] = [];
+  results?: ModuleIterationResultType[];
 
   @persist('object')
   @observable
